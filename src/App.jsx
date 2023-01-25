773b2bd6db0b9d82488e0da18d4380f99be0b585
praize-laurine <preyzelaurine@gmail.com>
2023-01-26 00:05:03 +0300

    Add the search term

--- src/App.jsx
+++ src/App.jsx
@@ -7,17 +7,18 @@ import SearchIcon from "./assets/search.svg";
 
 const API_URL = "https://www.omdbapi.com?apikey=b53e79ff";
 
-const movies1 = 
-{
-  "Title": "The Blacklist: Redemption",
-  "Year": "2017",
-  "imdbID": "tt5592230",
-  "Type": "series",
-  "Poster": "https://m.media-amazon.com/images/M/MV5BODI2MjI4MzQ1OV5BMl5BanBnXkFtZTgwOTk2MDAyMTI@._V1_SX300.jpg"
-}
+// const movies1 = 
+// {
+//   "Title": "The Blacklist: Redemption",
+//   "Year": "2017",
+//   "imdbID": "tt5592230",
+//   "Type": "series",
+//   "Poster": "https://m.media-amazon.com/images/M/MV5BODI2MjI4MzQ1OV5BMl5BanBnXkFtZTgwOTk2MDAyMTI@._V1_SX300.jpg"
+// }
 
 const App = () => {
   const [movies, setMovies] = useState([]);
+  const [searchTerm, setSearchTerm] = useState('');
 
   const searchMovies = async (title) => {
     const response = await fetch(`${API_URL}&s=${title}`);
@@ -27,7 +28,7 @@ const App = () => {
   };
 
   useEffect(() => {
-    searchMovies("blacklist");
+    searchMovies("");
   }, []);
 
   return (
@@ -37,10 +38,10 @@ const App = () => {
       <div className="search">
         <input
           placeholder="Search for movies"
-          value="Superman"
-          onChange={() => {}}
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
         />
-        <img src={SearchIcon} alt="alt" onClick={() => {}} />
+        <img src={SearchIcon} alt="alt" onClick={() => searchMovies(searchTerm)} />
       </div>
 
       {
@@ -54,7 +55,7 @@ const App = () => {
         ):
         (
           <div className="empty">
-            <h2>Upss!!! no movies found</h2>
+            <h2>Upss!!! No movies found</h2>
           </div>
         )
       }
