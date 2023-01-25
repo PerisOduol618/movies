da8193025f4bc0b48150edf10a831dcb54c7e7c7
praize-laurine <preyzelaurine@gmail.com>
2023-01-25 17:23:32 +0300

    State

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,20 @@
+import { useState, useEffect } from 'react'
 import './App.css'
 
-const Person = (props) => {
-  return(
-  <>
-  <h2>Name: {props.name} </h2>
-  <h2>Last Name: {props.lastName}</h2>
-  <h2>Age: {props.age}</h2>
-  </>
-  )
-}
+
+
 
 const App = () => {
+  const [counter, setCounter] = useState(0);
+  useEffect(() => {
+    setCounter(79);
+  },[])
 
   return (
     <div className="App">
-      <Person name='Kevin'lastName='Doe' age='16' /> <br /> 
-      <Person name='Alice'  lastName='Mash' age={4+8}/>  <br />
+      <button onClick={ () => setCounter((prevCount) => prevCount -1)} >-</button>
+      <h1>{counter}</h1>
+      <button onClick={ () => setCounter((prevCount) => prevCount +1)} >+</button>
 
     </div>
   )
